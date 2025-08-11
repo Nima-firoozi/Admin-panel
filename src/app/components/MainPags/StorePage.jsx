b11f5ed4b781aea5c3a921f1473fc0f22ad7b387
Nima <162740707+Nima-firoozi@users.noreply.github.com>
2025-08-11 03:56:46 +0330

    Update StorePage.jsx

--- src/app/components/MainPags/StorePage.jsx
+++ src/app/components/MainPags/StorePage.jsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from "@mui/material";
+import { Box } from "@mui/material";
 import React from "react";
 import { Gauge, gaugeClasses } from "@mui/x-charts/Gauge";
 import { PieChart } from "@mui/x-charts/PieChart";
